a46755f629d9d4e3fcb98f41c0d24274bb07ee36
Mielilon <daria.mg354@mail.ru>
2020-11-20 21:07:02 +0300

    Сделана шапка

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 //= jquery.min.js
+//= owl.carousel.js
+var owl = $(".owl-carousel");
+
+owl.owlCarousel({
+    loop: true,
+    nav: false,
+    margin: 0,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+    },
+});
