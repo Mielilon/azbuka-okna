0269ef1568c2af792c830e0b522edadc27a2b4df
Mielilon <daria.mg354@mail.ru>
2020-11-23 08:28:03 +0300

    Сделаны отзовы

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,46 @@
 //= jquery.min.js
 //= owl.carousel.js
-var owl = $(".owl-carousel");
+var owlWelcome = $(".owl-carousel_welcome");
 
-owl.owlCarousel({
+owlWelcome.owlCarousel({
     loop: true,
     nav: false,
     margin: 0,
     dots: true,
     autoplay: true,
+    smartSpeed: 500,
     autoplayTimeout: 3000,
     autoplayHoverPause: true,
     responsive: {
         0: {
             items: 1,
+        },
+    },
+});
+
+
+
+var owlWithArrows = $(".owl-carousel_with-arrows");
+
+owlWithArrows.owlCarousel({
+    loop: true,
+    nav: true,
+    navText: ['',''],
+    margin: 16,
+    dots: false,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        400: {
+            items: 2,
+        },
+        800: {
+            items: 3,
+        },
+        1200: {
+            items: 5,
         },
     },
 });
